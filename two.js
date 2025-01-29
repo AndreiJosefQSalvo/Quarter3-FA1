d17fd6fadc3164a5da84d5981840166ab24d233e
AndreiJosefQSalvo <andreijosef.salvo@smc.pshs.edu.ph>
2025-01-29 17:08:55 +0800

    Update two.js

--- two.js
+++ two.js
@@ -1,7 +1,9 @@
-var first = prompt("Enter your first name: ");
-var last = prompt("Enter your last name: ");
-var birth = prompt("Enter your birth year: ");
-var age = 2025 - birth;
-
-document.getElementById("num2").innerHTML = "Hello " + first + " " + last + "! " +
-"How does it feel to be " + age + " years old?";
+function bFunction() {
+	var first = prompt("Enter your first name: ");
+	var last = prompt("Enter your last name: ");
+	var birth = prompt("Enter your birth year: ");
+	var age = 2025 - birth;
+	
+	document.getElementById("num2").innerHTML = "Hello " + first + " " + last + "! " +
+	"How does it feel to be " + age + " years old?";
+}
