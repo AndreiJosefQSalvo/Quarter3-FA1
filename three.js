fc09a59b868160238b2ba35a9bc873947068996d
AndreiJosefQSalvo <andreijosef.salvo@smc.pshs.edu.ph>
2025-01-29 17:09:18 +0800

    Update three.js

--- three.js
+++ three.js
@@ -1,4 +1,7 @@
-function aFunction(){
+function cFunction(){
+	var nn = "Andrei", hin = 67, wkg = 60;
+	var hf = hin / 12, hin2 = hin % 12, wlbs = wkg*2.20462;
+	
 	if(confirm("Do you agree to share your information with this site?")) {
 		console.log("Name: " + nn);
 		console.log("Height: " + parseInt(hf) + "\'" + hin2 + "\"");
